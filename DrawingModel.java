647646b64cfd145773c9ff730d63687157288c1f
zhenqixu <33837569+zhenqixu@users.noreply.github.com>
2018-02-09 14:17:11 -0500

    add SuCircle and WuCircle in init

--- DrawingModel.java
+++ DrawingModel.java
@@ -27,7 +27,7 @@ public class DrawingModel {
 	*/
 	private void init(){
 		CircleShape c;
-		for(int i=0; i< circles.length/2; i++){
+		for(int i=0; i< circles.length/4; i++){
 			// we go to circles.length/2 as we add 2 different kinds of circles
 			// each time through this loop.. with 5 classes you'll divide by 5
 			// WHEN YOU ADD ANOTHER SUBCLASS OF CIRCLESHAPE YOU NEED TO
@@ -35,9 +35,14 @@ public class DrawingModel {
 			c = new CircleShape();
 			this.add(c);
 
-      c = new RadiusChangingCircle();
-      this.add(c);
-
+            c = new RadiusChangingCircle();
+            this.add(c);
+	
+	    c = new SuCircle();
+	    this.add(c);		
+             
+	    c = new WuCircle();
+	    this.add(c);
 		}
 	}
 
