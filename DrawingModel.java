69293bf0e61dc2b0b77185196824890cdc04fbda
acealex5 <33837475+acealex5@users.noreply.github.com>
2018-02-12 23:14:30 -0500

    Update DrawingModel.java

--- DrawingModel.java
+++ DrawingModel.java
@@ -27,7 +27,7 @@ public class DrawingModel {
 	*/
 	private void init(){
 		CircleShape c;
-		for(int i=0; i< circles.length/4; i++){
+		for(int i=0; i< circles.length/6; i++){
 			// we go to circles.length/2 as we add 2 different kinds of circles
 			// each time through this loop.. with 5 classes you'll divide by 5
 			// WHEN YOU ADD ANOTHER SUBCLASS OF CIRCLESHAPE YOU NEED TO
@@ -43,6 +43,14 @@ public class DrawingModel {
              
 	    c = new WuCircle();
 	    this.add(c);
+			
+	    c = new AlexCircle();
+	    this.add(c);
+			
+	    c = new QiCircle();
+	    this.add(c);		
+		
+			
 		}
 	}
 
