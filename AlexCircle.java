3204c756d26665f341c67e66bf3903b67cb19498
acealex5 <33837475+acealex5@users.noreply.github.com>
2018-02-12 23:13:18 -0500

    Completed

--- AlexCircle.java
+++ AlexCircle.java
@@ -2,10 +2,10 @@ package pa03;
 import java.awt.Color;
 
 public class AlexCircle extends CircleShape {
-	private double spdFactor = 10;
+	private double spdFactor = 10.0;
 	public AlexCircle() {
 		super();
-		this.color = new Color(0,100,255);
+		this.color = new Color(0,100,255,100);
 	}
 	
 	public static void main(String[] args) {
@@ -17,7 +17,7 @@ public class AlexCircle extends CircleShape {
 	}
 	
 	/**
-	 * I want to make the circles vary in speeds depending on which side it hits
+	 * I want to make the circles vary in speeds depending on which side it hits, the upper left sides speed it up, the lower right sides slow it down
 	 */
 	
 	  public void keepOnBoard(){
